2e70f56c13cd0c2a3b10f01fedd10c039ec525ec
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2022-11-05 21:33:08 +0000

    adicionando arquivos

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import scrapy
 
 class FatecBot(scrapy.Spider):
     name = "Fatec Bot"
-    start_urls = ["https://www.alura.com.br/cursos-online-programacao"]
-    def parse(self,response):
-        SELETOR = ".subcategoria__item"
-        cursos = []
-        for categoria in response.css(SELETOR):
-            curso = {}
+    urls = ["https://www.alura.com.br/cursos-online-programacao", 
+    "https://www.alura.com.br/cursos-online-front-end"]
 
-            NOME_SELETOR = ".card-curso__nome ::text"
-            LINK_SELETOR = ".card-curso ::attr(href)"
+    for url in start_urls:
+    
+        
+        def parse(self,response):
+            SELETOR = ".subcategoria__item"
+            cursos = []
 
-            curso['nome']= categoria.css(NOME_SELETOR).extract_first()
-            curso['link']= "https://www.alura.com.br/" + categoria.css(LINK_SELETOR).extract_first()
-            print(curso)
+            for categoria in response.css(SELETOR):
+                curso = {}
+
+                NOME_SELETOR = ".card-curso__nome ::text"
+                LINK_SELETOR = ".card-curso ::attr(href)"
+
+                curso['nome']= categoria.css(NOME_SELETOR).extract_first()
+                curso['link']= "https://www.alura.com.br/" + categoria.css(LINK_SELETOR).extract_first()
+                
+                print(curso)
+               
+
+                
+                
+                cursos.append(curso)
             
-            cursos.append(curso)
+            print("Quantidade de Cursos:", len(cursos))
 
         
 
